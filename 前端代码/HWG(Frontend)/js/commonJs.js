2989f52053bb8b26c5d64162d4a808c0defa3329
My_hjiong <912123784@qq.com>
2018-12-14 18:16:30 +0800

    黄炯 提交前端项目

---
+++ 前端代码/HWG(Frontend)/js/commonJs.js
@@ -0,0 +1,9 @@
+document.write('<script src="assets/js/jquery-1.12.3.min.js"></script>');
+document.write('<script type="text/javascript" src="assets/js/bootstrap.min.js"></script>');
+document.write('<script src="assets/vendors/modernizr/modernizr-2.6.2.min.js"></script>');
+document.write('<script type="text/javascript" src="assets/vendors/owl-carousel/owl.carousel.min.js"></script>');
+document.write('<script type="text/javascript" src="assets/vendors/flexslider/jquery.flexslider-min.js"></script>');
+document.write('<script type="text/javascript" src="assets/vendors/countdown/jquery.countdown.js"></script>');
+document.write('<script type="text/javascript" src="assets/js/main.js"></script>');
+
+
